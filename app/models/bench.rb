6ce1b82192d993026c54c38d548f8ddc4d7dc879
App Academy Student <student@appacademy.io>
2015-12-12 14:28:58 -0800

    got markers working right

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -2,7 +2,8 @@ class Bench < ActiveRecord::Base
   validates :description, :lat, :lng, presence: true
 
   def self.in_bounds(bounds)
-    self.where(lat: bounds['southWest']['lat'] .. bounds['northEast']['lat'])
+    self.where(lat: bounds['southWest']['lat'] .. bounds['northEast']['lat'],
+    lng: bounds['southWest']['lng'] .. bounds['northEast']['lng'])
   end
 end
 
